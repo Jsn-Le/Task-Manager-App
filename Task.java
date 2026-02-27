48941c595865030d932034674e9078122e8a5c43
Jason Le <jason.le0804@gmail.com>
2026-02-26 22:13:57 -0600

    created task model

--- Task.java
+++ Task.java
@@ -1,3 +1,45 @@
 public class Task {
 
+    private int id;
+    private String title;
+    private boolean completed;
+
+    public Task (int id, String title) {
+
+        this.id = id;
+        this.title = title;
+        this.completed = false;
+
+    }
+
+    public int getId() {
+
+        return id;
+
+    }
+
+    public String getTitle() {
+
+        return title;
+
+    }
+
+    public boolean isCompleted() {
+
+        return completed;
+
+    }
+
+    public void setTitle(String title) {
+
+        this.title = title;
+
+    }
+
+    public void markCompleted() {
+
+        this.completed = true;
+
+    }
+
 }
